af6a69e6e57926ecd898c2933b9e6590256d8b36
viktor <viktorkarlsson125@gmail.com>
2020-10-15 15:09:19 +0200

    la till meddelande om under-mellan o över-vikt, fixade ett fel så testet funkar. kan dock inte använda sidan än..

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -3,6 +3,7 @@ const calculateBMI = () => {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
     let results = calculateMetricBMI(weightInput.value, heightInput.value)
+    let message = getBMIClassification(results)
     let displayResultsElement = document.getElementById("results")
-    displayResultsElement.innerHTML = `<h1> your BMI value is: ${results}</h1>`
+    displayResultsElement.innerHTML = `<h1> your BMI value is: ${results} and you are ${message}</h1>`
 }
